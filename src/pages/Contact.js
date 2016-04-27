4c9678530cd83a20b264ea9a69928e92e8c68f24
daniel.pagharion <dpaghari@gmail.com>
2016-04-26 23:02:54 -0700

    Changed background Removed Hobbies, My Thoughts on Things for now Started Contact form Removed social media and replaced with github and resume links Restyled about and base styles Switched from scss to sass Using bourbon

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,6 +10,15 @@ export default class Contact extends React.Component {
     return (
         <section class="wrapper container">
           <h1>Contact</h1>
+          <form>
+            <label for="name">Name:</label>
+            <input type="text" name="name"></input><br/>
+            <label for="email">Email:</label>
+            <input type="text"></input><br/>
+            <label for="message">Message:</label>
+            <textarea></textarea><br/>
+            <input type="submit"></input>
+          </form>
         </section>
     );
   }
