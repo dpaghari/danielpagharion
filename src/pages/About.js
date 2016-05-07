122b85b51e371c5f300781064467b844ffef16cd
Daniel Pagharion <dpaghari@gmail.com>
2016-05-07 01:17:44 -0700

    Uhhh I think I added stuff

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,11 +10,13 @@ export default class About extends React.Component {
     return (
         <section class="wrapper" id="main">
           <div id="about">
-            <img src="../img/profilepic.jpg"/>
+            <div class="photo-lockup">
+              <img src="../img/profilepic.jpg"/>
+            </div>
             <div class="about-desc">
-              <h1>Just trying to help make the world a better place one line of code at a time...</h1>
+              <h1>Hey There! I'm Dan</h1>
               <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              I am currently a <span style={{color: "orange"}}>Front-End Web Developer</span> in San Francisco, CA.
               </p>
             </div>
           </div>
---
+++ src/pages/Photos.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class Photos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <section class="wrapper">
+        <div class="container">
+          <h1>Photos</h1>
+        </div>
+        <div class="container row">
+            <div class="project col-md-3">Playlistr</div>
+            <div class="project col-md-3">Nocturnum</div>
+            <div class="project col-md-3">Project Warp</div>
+        </div>
+      </section>
+    );
+  }
+}
